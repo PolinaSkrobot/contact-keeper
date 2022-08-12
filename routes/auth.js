292fc7a68ea3284905abb6208a5fd9cf85694a10
Polina Skrobot <chi.polly.no@gmail.com>
2022-08-12 16:59:40 -0700

    auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const auth = require("../middleware/auth");
 // route        GET api/route
 // description  Get logged in user
 // access       private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
-});
+router.post(
+  "/",
+  body("email", "please, include a valid email").isEmail(),
+  body("password", "password is required").exists(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 // route        POST api/route
 // description  Auth user & get token
 // access       public
-router.post("/", (req, res) => {
-  res.send("Log in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("server error");
+  }
 });
 module.exports = router;
