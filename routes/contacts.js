f578ff79a7d596278378093b599aa89669e79928
Polina Skrobot <chi.polly.no@gmail.com>
2022-08-13 12:25:08 -0700

    Pdate/delete contact request

--- routes/contacts.js
+++ routes/contacts.js
@@ -49,13 +49,51 @@ router.post(
 // route        PUT api/contacts/:id
 // description  Update contact
 // access       private
-router.put("/:id", (req, res) => {
-  res.send("Update contact");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+  //Build new contact object
+  const contactFileds = {};
+  if (name) contactFileds.name = name;
+  if (email) contactFileds.email = email;
+  if (phone) contactFileds.phone = phone;
+  if (type) contactFileds.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    //Check that user is owner of the contact:
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: contactFileds,
+      },
+      { new: true }
+    );
+    res.json(contact);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
 });
 // route        DELETE api/contacts/:id
 // description  Delete contact
 // access       private
-router.delete("/:id", (req, res) => {
-  res.send("Delete contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    //Check that user is owner of the contact:
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" });
+    }
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Contact deleted" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
 });
 module.exports = router;
