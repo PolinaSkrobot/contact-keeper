11fe8bc46503764562e991e1cc138bb289233041
Polina Skrobot <chi.polly.no@gmail.com>
2022-08-13 11:22:23 -0700

    Add contact request

--- routes/contacts.js
+++ routes/contacts.js
@@ -21,9 +21,31 @@ router.get("/", auth, async (req, res) => {
 // route        POST api/contacts
 // description  Add new contact
 // access       private
-router.post("/", (req, res) => {
-  res.send("Add contact");
-});
+router.post(
+  "/",
+  [auth, body("name", "please, add name").notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 // route        PUT api/contacts/:id
 // description  Update contact
 // access       private
