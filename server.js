68043e92c9ef654f6bcd60788dd5d6325d83c637
Polina Skrobot <chi.polly.no@gmail.com>
2022-08-13 18:03:45 -0700

    Client side installed

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => res.json({ msg: "Welcom to the Contact" }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
