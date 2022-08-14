69156b55ab4ebc5d12217f3900773048ce35cf36
Polina Skrobot <chi.polly.no@gmail.com>
2022-08-14 14:50:19 -0700

    Registration alerts

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,32 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Routes>
-                <Route exact path='/' element={<Home></Home>} />
-                <Route exact path='/about' element={<About></About>} />
-                <Route exact path='/register' element={<Register />} />
-                <Route exact path='/login' element={<Login />} />
-              </Routes>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Routes>
+                  <Route exact path='/' element={<Home></Home>} />
+                  <Route exact path='/about' element={<About></About>} />
+                  <Route exact path='/register' element={<Register />} />
+                  <Route exact path='/login' element={<Login />} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -9,12 +10,20 @@ const Register = () => {
   });
 
   const { name, email, password, password2 } = user;
+
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
   const onChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Reg submit");
+    if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      console.log("Reg submit");
+    }
   };
   return (
     <div className='form-container'>
@@ -24,11 +33,23 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' value={name} onChange={onChange} />
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='email'>Email Address</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='passsword'>Password</label>
@@ -37,6 +58,8 @@ const Register = () => {
             name='password'
             value={password}
             onChange={onChange}
+            required
+            minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -46,6 +69,8 @@ const Register = () => {
             name='password2'
             value={password2}
             onChange={onChange}
+            required
+            minLength='6'
           />
         </div>
         <input
